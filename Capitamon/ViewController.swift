cc8a37af4da20d7fd5157a1907beb6a9bcd571fb
Meyrillan <meyrillan@gmail.com>
2020-08-26 18:13:40 -0300

    First commit

--- Capitamon/ViewController.swift
+++ Capitamon/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pokemonUm: UITextField!
+    @IBOutlet weak var pokemonDois: UITextField!
+    @IBOutlet weak var pokemonTres: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func btnCalculate(_ sender: Any) {
+        
+    }
+    
 
 }
 
