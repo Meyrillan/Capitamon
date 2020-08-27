de353149678fc536ee37a457a13861d3f247f331
Felipe <felipebandeira.eu@gmail.com>
2020-08-27 15:37:04 -0300

    Preparei as três tasks e fiz elas funcionarem individualmente

--- Capitamon/ViewController.swift
+++ Capitamon/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var pokemonUm: UITextField!
     @IBOutlet weak var baseExperiencePokemonUm: UILabel!
+    @IBOutlet weak var baseExperiencePokemonDois: UILabel!
+    @IBOutlet weak var baseExperiencePokemonTres: UILabel!
     @IBOutlet weak var pokemonDois: UITextField!
     @IBOutlet weak var pokemonTres: UITextField!
     
@@ -25,17 +27,28 @@ class ViewController: UIViewController {
     
     @IBAction func btnCalculate(_ sender: Any) {
         
-        // Busca a experiencia de um pokemon usando o nome do pokemon que a pessoa preencheu em PokemonUM
+        // Busca a experiencia de um pokemon usando o nome do pokemon que a pessoa preencheu em PokemonUM, PokemonDOIS e PokemonTres
         let nomePokemonUm = self.pokemonUm.text
-        let stringURL = "https://pokeapi.co/api/v2/pokemon/\(nomePokemonUm!)"
-        let url = URL(string:stringURL)!
+        let nomePokemonDois = self.pokemonDois.text
+        let nomePokemonTres = self.pokemonTres.text
+        
+        let stringURL1 = "https://pokeapi.co/api/v2/pokemon/\(nomePokemonUm!)"
+        let stringURL2 = "https://pokeapi.co/api/v2/pokemon/\(nomePokemonDois!)"
+        let stringURL3 = "https://pokeapi.co/api/v2/pokemon/\(nomePokemonTres!)"
+        
+        let url1 = URL(string:stringURL1)!
+        let url2 = URL(string:stringURL2)!
+        let url3 = URL(string:stringURL3)!
+        
         let session = URLSession.shared
-        let task = session.dataTask(with: url) { data, response, serverError in
+        
+        let task1 = session.dataTask(with: url1) { data, response, serverError in
             do {
                 let decoder = JSONDecoder()
                 let jsonData = try decoder.decode(Pokemon.self, from: data!)
                 
                 DispatchQueue.main.async {
+                    
          //conversão de int pra string
                     let guardaInt = jsonData.baseExperience
                     let experiencePokemonUm = String(guardaInt)
@@ -49,7 +62,54 @@ class ViewController: UIViewController {
             }
         }
         
-        task.resume()
+        
+        let task2 = session.dataTask(with: url2) { data, response, serverError in
+            do {
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode(Pokemon.self, from: data!)
+                
+                DispatchQueue.main.async {
+                    
+         //conversão de int pra string
+                    let guardaInt = jsonData.baseExperience
+                    let experiencePokemonDois = String(guardaInt)
+                    self.baseExperiencePokemonDois.text = "Experiência: " + experiencePokemonDois
+                }
+            } catch {
+                print("JSON error: \(error.localizedDescription)")
+                DispatchQueue.main.async {
+                    self.baseExperiencePokemonDois.text = "Esse Pokemón não existe ou está escrito errado"
+                }
+            }
+        }
+        
+        let task3 = session.dataTask(with: url3) { data, response, serverError in
+            do {
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode(Pokemon.self, from: data!)
+                
+                DispatchQueue.main.async {
+                    
+         //conversão de int pra string
+                    let guardaInt = jsonData.baseExperience
+                    let experiencePokemonTres = String(guardaInt)
+                    self.baseExperiencePokemonTres.text = "Experiência: " + experiencePokemonTres
+                }
+            } catch {
+                print("JSON error: \(error.localizedDescription)")
+                DispatchQueue.main.async {
+                    self.baseExperiencePokemonTres.text = "Esse Pokemón não existe ou está escrito errado"
+                }
+            }
+        }
+        
+        task1.resume()
+        task2.resume()
+        task3.resume()
         
     }
 }
+
+//Dificuldades:
+//1) Como fazer as tasks retornarem valores inteiros pra que possamos somar e, assim, conseguir a experiência total?
+//2) Como fazer um laço pra otimizar o código e realizar só uma task, em vez de três?
